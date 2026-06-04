91e4ba811c2e33248aa96ff01a6b481307350afd
ByKoan <officialbykoan@gmail.com>
2026-06-04 14:55:52 +0200

    Fix: fixed bug at compile, and use GCC to compile

--- Compile.bat
+++ Compile.bat
@@ -1,3 +1,4 @@
-cmake -B build -DCMAKE_BUILD_TYPE=Release
-cmake --build build --config Release -j
+rmdir /s /q build
+cmake -B build -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DFETCHCONTENT_QUIET=OFF
+cmake --build build --config Release -j --verbose
 pause
